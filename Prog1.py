44358c1f96a42942079fc7600317f329fb46e237
VarunNalagandla <varunnalagandla@gmail.com>
2024-08-01 18:12:57 +0530

    Guessing game using Python code

--- Prog1.py
+++ Prog1.py
@@ -1,1 +1,18 @@
 #Guessing game using random module
+import random as a
+def guessgame():
+    c=a.randint(1,100)
+    count=0
+    while(True):
+        count=count+1
+        n=int(input(f"enter your guess No{count} : "))
+        if(n>c):
+            print("please enter smaller than",n)
+        elif(n<c):
+            print("please enter bigger than",n)
+        elif(n==c):
+            print("your guess is right the number is",c)
+            break
+        else:
+            print("invalid input")
+    print(f"you have guessed the number in {count} attempts")
